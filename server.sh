1cfaa890428b64324355f559ffbbb2447a4e9e8d
Carsten Bund <carstenbund@gmail.com>
2025-09-04 11:59:20 -0700

    server

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 series=000500
-series=000100
+series=000180
 network=cdf
 network=demo
 resolution=512
@@ -10,13 +10,13 @@ resolution=1024
 outdir=images/${network}/${resolution}/${series} \
 network_pkl=data/${network}/${resolution}/network-snapshot-${series}.pkl
 
-sudo docker run -it --rm --runtime nvidia \
+sudo docker run -d --rm --gpus all \
           -v `pwd`:/scratch --workdir /scratch \
-	  -p 5000:5000 \
+	  -p 5001:5000 \
           -e NVIDIA_VISIBLE_DEVICES=all \
           -e NVIDIA_DRIVER_CAPABILITIES=all \
 	  -e NETWORK_PKL=${network_pkl} \
-          -e HOME=/scratch stylegan_server  \
+          -e HOME=/scratch stylegan-server  \
           python3 stylegan_server.py --outdir=${outdir} \
                   --steps=10 \
                   --network_pkl=${network_pkl}
